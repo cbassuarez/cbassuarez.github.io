b2031b5fa4d5acd5d6899baf0655371458d61058
Seb Suarez <contact@cbassuarez.com>
2026-07-15 00:31:00 -0700

    fix

--- public/labs/chunk-surfer/src/platform/index.js
+++
@@ -1,8 +0,0 @@
-import { browserPlatform } from './browser.js';
-import { steamPlatform } from './steam.js';
-
-export function currentPlatform() {
-  return globalThis.window?.platformBridge ? steamPlatform : browserPlatform;
-}
-
-export const platform = currentPlatform();
